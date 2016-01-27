9ca34eb50f37bcd96baabce0729311c535e093ad
Daniel Drasdo <danieldrasdo@gmail.com>
2016-01-26 17:39:06 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# myContentPages
+
+[myContentPages](https://github.com/danieldrasdo/myContentPages) is used in setting up my local developing environment when creating content pages.
+
+**Prior to using [myContentPages](https://github.com/danieldrasdo/myContentPages), make sure Node.js version 4.2.3 or higher is installed. You can check the Node.js version you have installed by running the following command in your terminal.**
+
+	node -v
+
+##Install Packages
+
+After cloning [myContentPages](https://github.com/danieldrasdo/myContentPages) to your computer, `Terminal` into your project directory and run the following command to install all required node packages.\*
+
+	npm install
+
+\* *You may need to use `sudo npm install`. Also, it may take a bit to install the devDependencies.*
+
+##Start
+
+While still in your project directory, run the following command:
+
+	gulp
+
+If that doesn't work, chances are you don't have gulp installed globally, which gives you the availability of using `gulp` on the command line. Try using the following command to install gulp globally:
+
+	npm install -g gulp
+
+Then try running `gulp` again.
+
+
+####Notes
+
+1. Open the project folder in your IDE.
+2. The sass/js files within the `_src` directory are the files used while developing.
+3. The `index.html` inside the `production/` directory should be used for production.
+4. If you'd like, you could use Bower to grab the latest versions of jQuery, Angular, etc., but I personally like fetching those and linking them up in my html myself.
---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+// Include gulp and plugins
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    inlinesource = require('gulp-inline-source'),
+    htmlreplace = require('gulp-html-replace'),
+    reload = browserSync.reload;
+
+//Log Errors
+function errorLog(err) {
+  console.error(err.message);
+  this.emit('end');
+}
+
+//Scripts Task
+gulp.task('scripts', function() {
+  return gulp.src('_src/js/**/*.js')
+    .pipe(concat('functions.js'))
+    .on('error', errorLog)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(rename('functions.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'))
+
+    .pipe(reload({stream:true}));
+});
+
+//Styles Task
+gulp.task('styles', function() {
+  gulp.src('_src/sass/**/*.+(sass|scss)')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .on('error', errorLog)
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('assets/css'))
+
+    .pipe(reload({stream:true}));
+});
+
+//HTML Task
+gulp.task('html', function() {
+  gulp.src('./*.html')
+    .pipe(reload({stream:true}))
+    .pipe(inlinesource({
+      compress: false,
+      pretty: true
+    }))
+    .pipe(htmlreplace({
+        'production': ''
+    }))
+    .pipe(gulp.dest('./production'));
+});
+
+// BrowserSync
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./"
+    },
+    notify: false
+  });
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch('_src/js/**/*.js', ['scripts']);
+  gulp.watch('_src/sass/**/*.+(sass|scss)', ['styles']);
+  gulp.watch("./**/*.html", ['html']);
+});
+
+// Default Task
+gulp.task('default', ['scripts', 'styles', 'html', 'browser-sync', 'watch']);
